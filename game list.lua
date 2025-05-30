7e751568cc7cc37beb30feda1e6eb9bd4a41727b
Nova-Hub-Devs <novahub015@gmail.com>
2025-05-30 03:11:34 -0600

    Update game list.lua

--- game list.lua
+++ game list.lua
@@ -1,6 +1,6 @@
 local Games = {
 
-[126884695634066] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Fisch.lua", -- Grow a Garden
+[126884695634066] = "https://raw.githubusercontent.com/Nova-Hub-Devs/Nova-Hub/refs/heads/main/obfuscated_script-1748596166826.lua.txt", -- Grow a Garden
 
 }
 

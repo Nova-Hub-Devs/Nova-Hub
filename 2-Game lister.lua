a95ad18c86b4ea44983b301a570d3a4fc6f31202
Nova-Hub-Devs <novahub015@gmail.com>
2025-05-30 19:31:18 -0600

    Create 2-Game lister.lua

---
+++ 2-Game lister.lua
@@ -0,0 +1,7 @@
+local Games = {
+
+[16732694052] = "https://raw.githubusercontent.com/Nova-Hub-Devs/Nova-Hub/refs/heads/main/obfuscated_script-1748596166826.lua.txt", -- Grow a Garden
+
+}
+
+return Games

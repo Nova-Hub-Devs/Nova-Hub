3cc00483aff42f97cb66d4ed7e54fd0ae234fe7f
Nova-Hub-Devs <novahub015@gmail.com>
2025-05-30 19:32:49 -0600

    Update 2-Game lister.lua

--- 2-Game lister.lua
+++ 2-Game lister.lua
@@ -1,6 +1,6 @@
 local Games = {
 
-[16732694052] = "https://raw.githubusercontent.com/Nova-Hub-Devs/Nova-Hub/refs/heads/main/obfuscated_script-1748596166826.lua.txt", -- Grow a Garden
+[16732694052] = "https://raw.githubusercontent.com/Nova-Hub-Devs/Nova-Hub/refs/heads/main/pandadevelopment.netvirtualfile9325db733e051c89.lua", -- Fisch
 
 }
 
